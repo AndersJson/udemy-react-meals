53670c89b19649d5d15056b0aabe0df1591412a2
Anders Jonsson <andersh_hotmail.com>
2021-05-10 13:53:51 +0200

    add hero-section and welcome-card with css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import Card from "./components/Card";
+import classes from "./App.module.css";
 
 function App() {
 	return (
-		<div className="App">
+		<div className={classes.App}>
 			<Header />
+			<div className={classes.hero}></div>
+			<div className={classes.content}>
+				<Card className={classes["welcome-card"]}>
+					<h2>Welcome!</h2>
+				</Card>
+			</div>
 		</div>
 	);
 }
