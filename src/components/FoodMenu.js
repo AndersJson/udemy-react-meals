03ff3468b56d1cb45b0d27d3d8284f570ddd3f08
Anders Jonsson <andersh_hotmail.com>
2021-05-12 23:14:15 +0200

    add css to foodMenuItems

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -7,37 +7,54 @@ import classes from "./FoodMenu.module.css";
 const FoodMenu = () => {
 	const foodMenuObject = [
 		{
+			id: "m1",
 			title: "Mixed-Sushi",
 			description: "16 pices in a mix of our finest sushi.",
 			price: 20.15,
 		},
 		{
+			id: "m2",
 			title: "Poke-Bowl",
 			description: "Mixed asian sallad, salmon and sticky rice.",
 			price: 18.5,
 		},
 		{
+			id: "m3",
 			title: "Harry's Burger",
 			description:
 				"120g burger with pickled onions, chili-sauce and sweetpotatoe-fries.",
 			price: 16.85,
 		},
 		{
+			id: "m4",
 			title: "Kids Banger",
 			description: "Sausage, fries and lots of ketchup.",
 			price: 9.99,
 		},
 		{
+			id: "m5",
 			title: "Lasagna",
 			description: "Creamy weggie-lasagna.",
 			price: 13.85,
+		},
+		{
+			id: "m6",
+			title: "Soup de mare",
+			description: "Sweet chilli seafood-soup.",
+			price: 16.25,
+		},
+		{
+			id: "m7",
+			title: "Carbonara",
+			description: "Pasta carbonare de penne.",
+			price: 15.95,
 		},
 	];
 	return (
 		<Card className={classes["food-menu"]}>
 			<ul className={classes["food-menu__list"]}>
 				{foodMenuObject.map((course) => {
-					return <FoodMenuItem />;
+					return <FoodMenuItem key={course.id} />;
 				})}
 			</ul>
 		</Card>
--- src/components/FoodMenuItem.js
+++ src/components/FoodMenuItem.js
@@ -6,10 +6,13 @@ const FoodMenuItem = (props) => {
 	return (
 		<li className={classes["food-menu__item"]}>
 			<div className={classes["food-menu__info"]}>
-				<p>Info</p>
+				<p>Hamburger</p>
+				<p>Descripiton</p>
+				<p>Price</p>
 			</div>
 			<div className={classes["food-menu__action"]}>
-				<p>Action</p>
+				<p>Number</p>
+				<p>Button</p>
 			</div>
 		</li>
 	);
