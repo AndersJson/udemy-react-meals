a896ec808e0152c920e613bf3ab4aad18a726c48
Anders Jonsson <andersh_hotmail.com>
2021-05-17 09:26:31 +0200

    add svg-icon to cart and style cartButton

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -3,7 +3,7 @@ import React from "react";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
-	return <button>{props.children}</button>;
+	return <button className={classes["cart-button"]}>{props.children}</button>;
 };
 
 export default CartButton;
