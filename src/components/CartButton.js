c4acbf6a49c15b5ae7a1357ddfa9ef282fa57b5d
Anders Jonsson <andersh_hotmail.com>
2021-05-19 12:26:25 +0200

    move cart-context from cartCount to cartButton and pass props to cartCount

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "./CartIcon";
 import CartCount from "./CartCount";
+import CartContext from "../store/cart-context";
 
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+	const cartCtx = useContext(CartContext);
+	const cartItemCount = cartCtx.items.reduce((acc, current) => {
+		return acc + current.amount;
+	}, 0);
+
 	return (
 		<button className={classes["cart-button"]} onClick={props.onClick}>
 			<CartIcon />
 			<p>Your Cart</p>
-			<CartCount />
+			<CartCount cartItemAmount={cartItemCount} />
 		</button>
 	);
 };
--- src/components/CartCount.js
+++ src/components/CartCount.js
@@ -1,21 +1,9 @@
-import React, { useContext } from "react";
-import CartContext from "../store/cart-context";
+import React from "react";
 
 import classes from "./CartCount.module.css";
 
-const CartCount = () => {
-	const cartCtx = useContext(CartContext);
-	let cartItemCount = 0;
-
-	if (cartCtx.items.length > 0) {
-		cartItemCount = cartCtx.items
-			.map((item) => item.amount)
-			.reduce((acc, current) => {
-				return acc + current;
-			}, 0);
-	}
-
-	return <span className={classes["cart-count"]}>{cartItemCount}</span>;
+const CartCount = (props) => {
+	return <span className={classes["cart-count"]}>{props.cartItemAmount}</span>;
 };
 
 export default CartCount;
