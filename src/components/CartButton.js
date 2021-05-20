816fb60445d19ed01c90a92257ae1549df31ca99
Anders Jonsson <andersh_hotmail.com>
2021-05-20 23:25:05 +0200

    add code to cart reduce-function to group courses and increase amount when adding new

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import CartCount from "./CartCount";
 import CartContext from "../store/cart-context";
@@ -7,13 +7,36 @@ import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
 	const cartCtx = useContext(CartContext);
+	const [isCartButtonHighlighted, setIsCartButtonHighlighted] = useState(false);
 
-	const cartItemCount = cartCtx.items.reduce((acc, current) => {
+	const { items } = cartCtx;
+
+	const cartItemCount = items.reduce((acc, current) => {
 		return acc + current.amount;
 	}, 0);
 
+	const btnClasses = `${classes["cart-button"]} ${
+		isCartButtonHighlighted ? classes.bump : ""
+	}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+
+		setIsCartButtonHighlighted(true);
+
+		let timer = setTimeout(() => {
+			setIsCartButtonHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes["cart-button"]} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<CartIcon />
 			<p>Your Cart</p>
 			<CartCount cartItemAmount={cartItemCount} />
--- src/components/CartListItem.js
+++ src/components/CartListItem.js
@@ -4,14 +4,18 @@ import classes from "./CartListItem.module.css";
 
 const CartListItem = (props) => {
 	const price = `$${props.price.toFixed(2)}`;
-
 	return (
 		<li className={classes["cart-item"]}>
 			<div className={classes["cart-item__info"]}>
 				<p className={classes["cart-item__title"]}>{props.title}</p>
-				<p className={classes["cart-item__price"]}>{price}</p>
+				<span className={classes["cart-item__price-wrapper"]}>
+					<p className={classes["cart-item__price"]}>{price}</p>
+					<span
+						className={classes["cart-item__amount"]}
+					>{`x${props.amount}`}</span>
+				</span>
 			</div>
-			<div className={classes["cart-item__action"]}>The input</div>
+			<div className={classes["cart-item__action"]}>+ / -</div>
 		</li>
 	);
 };
